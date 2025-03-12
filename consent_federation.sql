762564dea9616d9dd341347df84725b875d4f316
Manjesh Vinayaka <manjesh.vinayaka@thoughtworks.com>
2025-03-12 19:38:57 +0530

    created 2 schemas
    1. consent
    public for students

---
+++ consent_federation.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+
+CREATE SERVER remote_pg_server
+FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (host 'consent', dbname 'postgres', port '5432');
+
+
+CREATE USER MAPPING FOR current_user
+SERVER remote_pg_server
+OPTIONS (user 'postgres', password 'postgres');
+
+IMPORT FOREIGN SCHEMA public
+FROM SERVER remote_pg_server
+INTO consent;
+
+DROP SERVER IF EXISTS remote_pg_server CASCADE;
---
+++ student_federation.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION IF NOT EXISTS mysql_fdw;
+
+CREATE SERVER mysql_student_db
+FOREIGN DATA WRAPPER mysql_fdw
+OPTIONS (host 'student', port '3306');
+
+
+CREATE ROLE reporting_user WITH LOGIN PASSWORD 'rootpassword';
+
+
+CREATE USER MAPPING FOR reporting_user
+SERVER mysql_student_db
+OPTIONS (username 'root', password 'rootpassword');
+
+
+CREATE FOREIGN TABLE order_details (
+    OrderDetailID INTEGER,
+    OrderID INTEGER,
+    ProductID INTEGER,
+    Quantity INTEGER
+) SERVER mysql_student_db
+OPTIONS (dbname 'my_database', table_name 'OrderDetails');
+
+drop FOREIGN TABLE order_details;
+
+SELECT * FROM order_details;
+
+
+-- ### Root user
+GRANT USAGE ON FOREIGN SERVER mysql_student_db TO reporting_user;
+
+grant all on table order_details to reporting_user;
+
+GRANT USAGE ON FOREIGN DATA WRAPPER mysql_fdw TO reporting_user;
